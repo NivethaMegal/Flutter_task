a8f1a7dce6c27eadaf75b327c7eff4449c92fa30
NivethaMegal <nivetha.ramesh.0599@gmail.com>
2020-12-31 14:41:31 +0530

    callback function

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_task/resources/images.dart';
 import 'package:flutter_task/screens/grade.dart';
 import 'package:flutter_task/screens/menu.dart';
 import 'package:flutter_task/screens/plans_and_usage.dart';
+import 'package:flutter_task/widgets/bottom_navigationbar.dart';
 import 'package:flutter_task/widgets/drawer.dart';
 import 'package:flutter_task/screens/dashboard.dart';
 import 'package:flutter_task/themes/colors.dart';
@@ -46,47 +47,12 @@ class AppState extends State<App> {
       ),
       drawer: AppDrawer(),
       body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          onTap: _selectPage,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: ImageIcon(
-                AssetImage(Images.DASHBOARD),
-                color: AppColors.COLOR_BLUE,
-              ),
-              label: 'Dashboard',
-              backgroundColor: AppColors.COLOR_WHITE,
-            ),
-            BottomNavigationBarItem(
-              icon: ImageIcon(
-                AssetImage(Images.DOCUMENT),
-                color: AppColors.COLOR_BLUE,
-              ),
-              label: 'Plans & Usage',
-              backgroundColor: AppColors.COLOR_WHITE,
-            ),
-            BottomNavigationBarItem(
-              icon: ImageIcon(
-                AssetImage(Images.STAR),
-                color: AppColors.COLOR_BLUE,
-              ),
-              label: 'Promos',
-              backgroundColor: AppColors.COLOR_WHITE,
-            ),
-            BottomNavigationBarItem(
-              icon: ImageIcon(
-                AssetImage(Images.MENU),
-                color: AppColors.COLOR_BLUE,
-              ),
-              label: 'Menu',
-              backgroundColor: AppColors.COLOR_WHITE,
-            ),
-          ],
-          type: BottomNavigationBarType.shifting,
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.black,
-          iconSize: 30,
-          elevation: 5),
+      bottomNavigationBar: BottomNavigation(
+        selectPage: (int index) {
+          _selectPage(index);
+        },
+        labelIndex: _selectedIndex,
+      ),
       floatingActionButton: new FloatingActionButton(
         backgroundColor: AppColors.COLOR_BLACK,
         onPressed: () {},
--- lib/widgets/bottom_navigationbar.dart
+++ lib/widgets/bottom_navigationbar.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task/resources/images.dart';
+import 'package:flutter_task/themes/colors.dart';
 
+typedef void IntCallback(int _selectedIndex);
+
+class BottomNavigation extends StatelessWidget {
+  final IntCallback selectPage;
+  int labelIndex;
+  BottomNavigation({@required this.selectPage, @required this.labelIndex});
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        onTap: selectPage,
+        currentIndex: labelIndex,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage(Images.DASHBOARD),
+              color: AppColors.COLOR_BLUE,
+            ),
+            label: 'Dashboard',
+            backgroundColor: AppColors.COLOR_WHITE,
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage(Images.DOCUMENT),
+              color: AppColors.COLOR_BLUE,
+            ),
+            label: 'Plans & Usage',
+            backgroundColor: AppColors.COLOR_WHITE,
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage(Images.STAR),
+              color: AppColors.COLOR_BLUE,
+            ),
+            label: 'Promos',
+            backgroundColor: AppColors.COLOR_WHITE,
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage(Images.MENU),
+              color: AppColors.COLOR_BLUE,
+            ),
+            label: 'Menu',
+            backgroundColor: AppColors.COLOR_WHITE,
+          ),
+        ],
+        type: BottomNavigationBarType.shifting,
+        selectedItemColor: Colors.black,
+        iconSize: 30,
+        elevation: 5);
+  }
+}
